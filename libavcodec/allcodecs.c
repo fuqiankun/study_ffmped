b3ab544f0f4b6e7dc21ae6c929ece7d723a824a3
qiankun <qiankun@whale>
2020-03-23 16:46:40 +0800

    format

--- libavcodec/allcodecs.c
+++ libavcodec/allcodecs.c
@@ -4,16 +4,14 @@ extern AVCodec truespeech_decoder;
 extern AVCodec msrle_decoder;
 
 // 简单的注册/初始化函数，把编解码器用相应的链表串起来便于查找识别。
-void avcodec_register_all(void)
-{
-    static int inited = 0;
+void avcodec_register_all(void) {
+	static int inited = 0;
 
-    if (inited != 0)
-	return;
+	if (inited != 0) return;
 
-    inited = 1;
-    // 把msrle_decoder 解码器串接到解码器链表，链表头指针是first_avcodec。
-    register_avcodec(&msrle_decoder);
-    // 把truespeech_decoder 解码器串接到解码器链表，链表头指针是first_avcodec。
-    register_avcodec(&truespeech_decoder);
+	inited = 1;
+	// 把msrle_decoder 解码器串接到解码器链表，链表头指针是first_avcodec。
+	register_avcodec(&msrle_decoder);
+	// 把truespeech_decoder 解码器串接到解码器链表，链表头指针是first_avcodec。
+	register_avcodec(&truespeech_decoder);
 }
--- libavutil/rational.h
+++ libavutil/rational.h
@@ -1,15 +1,11 @@
 #ifndef RATIONAL_H
 #define RATIONAL_H
 
-typedef struct AVRational
-{
-    int num; // numerator   // 分子
-    int den; // denominator // 分母
+typedef struct AVRational {
+	int num;	// numerator   // 分子
+	int den;	// denominator // 分母
 } AVRational;
 
-static inline double av_q2d(AVRational a)
-{
-    return a.num / (double)a.den;
-}
+static inline double av_q2d(AVRational a) { return a.num / (double)a.den; }
 
 #endif
